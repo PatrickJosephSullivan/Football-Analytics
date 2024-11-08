ab77c80fce5955f8ab88595e69936f9e00525965
PatrickJosephSullivan <101905657+PatrickJosephSullivan@users.noreply.github.com>
2024-11-08 14:59:52 -0800

    Update NFL_data_import.py
    Added comments and deleted unnecessary code.

--- Stable/NFL_data_import.py
+++ Stable/NFL_data_import.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 # Currently the oldest player in the NFL is Aaron Rodgers as of the 2024-2025 Season
 # Thus we only pull data from 2005 season onward
+# Variables are in df format
 player_season_data = nfl.import_seasonal_data(range(2005,2024), s_type='REG')
 player_info = nfl.import_players()
 player_ids = nfl.import_ids()
 
+# Dumps dfs to the project folder as Excel to familiarize with the data
 player_season_data.to_excel('Player Season Data.xlsx')
 player_info.to_excel('Player Info.xlsx')
 player_ids.to_excel('Player Ids.xlsx')
 
+# Dumps dfs to the project folder as CSV for SQL ingestion
 player_season_data.to_csv('Player_Season_Data.csv')
 player_info.to_csv('Player_Info.csv')
 player_ids.to_csv('Player_Ids.csv')
-
-# pd.DataFrame(nfl.import_seasonal_data([2024], s_type='REG'))
