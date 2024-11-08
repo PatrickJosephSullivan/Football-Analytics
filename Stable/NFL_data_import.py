dc5dbce38c8b6356e2493cb0dd8c446f8fb0bc83
PatrickJosephSullivan <101905657+PatrickJosephSullivan@users.noreply.github.com>
2024-11-08 15:12:56 -0800

    Updated and Ran NFL_data_import.py

--- Stable/NFL_data_import.py
+++ Stable/NFL_data_import.py
@@ -1,5 +1,6 @@
 import nfl_data_py as nfl
 import pandas as pd
+import os
 
 # Currently the oldest player in the NFL is Aaron Rodgers as of the 2024-2025 Season
 # Thus we only pull data from 2005 season onward
@@ -9,11 +10,11 @@ player_info = nfl.import_players()
 player_ids = nfl.import_ids()
 
 # Dumps dfs to the project folder as Excel to familiarize with the data
-player_season_data.to_excel('Player Season Data.xlsx')
-player_info.to_excel('Player Info.xlsx')
-player_ids.to_excel('Player Ids.xlsx')
+player_season_data.to_excel(os.path.join('Stable', 'Player_CSVs', 'Player Season Data.xlsx'))
+player_info.to_excel(os.path.join('Stable', 'Player_CSVs', 'Player Info.xlsx'))
+player_ids.to_excel(os.path.join('Stable', 'Player_CSVs', 'Player Ids.xlsx'))
 
 # Dumps dfs to the project folder as CSV for SQL ingestion
-player_season_data.to_csv('Player_Season_Data.csv')
-player_info.to_csv('Player_Info.csv')
-player_ids.to_csv('Player_Ids.csv')
+player_season_data.to_csv(os.path.join('Stable', 'Player_CSVs', 'Player_Season_Data.csv'))
+player_info.to_csv(os.path.join('Stable', 'Player_CSVs', 'Player_Info.csv'))
+player_ids.to_csv(os.path.join('Stable', 'Player_CSVs', 'Player_Ids.csv'))
