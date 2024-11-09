b44cd12a4ccda77c73a9f64e7bd53e3cabaa7da0
PatrickJosephSullivan <101905657+PatrickJosephSullivan@users.noreply.github.com>
2024-11-08 16:08:22 -0800

    Created DB and cursor

--- Stable/SQL_Table_Upload.py
+++ Stable/SQL_Table_Upload.py
@@ -2,9 +2,13 @@ import sqlite3
 import pandas as pd
 import os
 
-with open('Player_Ids.csv', 'r') as file:
+con = sqlite3.connect("player_database.db")
+cur = con.cursor()
+
+with open(os.path.join('Stable', 'Player_CSVs', 'Player_Season_Data.csv'), 'r') as file:
     # player_ids = file.read()
     player_ids = pd.read_csv(file)
+
 
 def upload_player_ids(dataframe, upload_table, append=False, replace=True):
     pass
