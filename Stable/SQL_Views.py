f202ae6da630d8fd897bb8f08ffe413ad0f026b6
PatrickJosephSullivan <101905657+PatrickJosephSullivan@users.noreply.github.com>
2024-11-10 16:38:08 -0800

    Added the all_active_player_stats function
    Added a view that will be the basis of a lot of analysis. Planning to include many more views.

---
+++ Stable/SQL_Views.py
@@ -0,0 +1,30 @@
+import sqlite3
+import os
+
+db_name = "player_database"
+db_file_path = os.path.join("Stable","Database_files")
+db_file_string = os.path.join(db_file_path, f'{db_name}.db')
+
+con = sqlite3.connect(db_file_string)
+cur = con.cursor()
+
+def all_active_players_stats():
+    all_stats = cur.execute("""
+    WITH max_season AS (
+    SELECT player_id, MAX(season) max_season FROM player_season GROUP BY player_id HAVING MAX(season >= 2024)
+    )
+
+    SELECT ids.gsis_id, ids.name, info.first_name, info.last_name, info.status, info.team_abbr, info.position, season.season, season.games, season.completions, season.attempts, season.passing_yards,
+    season.passing_tds, season.interceptions, season.sacks, season.carries, season.rushing_yards, season.rushing_tds, season.receptions, season.receiving_yards, season.receiving_tds,
+    season.special_teams_tds, season.fantasy_points
+    FROM player_ids ids
+    LEFT JOIN player_info info ON ids.gsis_id = info.gsis_id
+    LEFT JOIN player_season season ON ids.gsis_id = season.player_id
+    LEFT JOIN max_season ON ids.gsis_id = max_season.player_id
+    WHERE max_season.max_season = 2024
+    ORDER BY team_abbr, name, season
+    ;""")
+    return all_stats
+
+# all_player_ids = cur.execute('SELECT gs FROM player_ids')
+# print(all_player_ids.fetchone())
